c29a34d82082989b8e12d26d48018e2bcce29e7b
ShakeelaLab <shakeela@live.nl>
2025-11-06 20:59:18 +0100

    6-11

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,18 @@ import './App.css';
 
 function App() {
   return (
-    <h1>Begin hier met met maken van de applicatie!</h1>
+      <>
+    <h1>Tech it easy dashboard</h1>
+      <h3>Verkoopoverzicht</h3>
+        <div className="inner-container">
+        <div className="div-article">
+        <article className="article">Aantal verkochte producten</article>
+        <article className="article">Aantal ingekochte producten</article>
+        <article className="article">Aantal te verkopen producten</article>
+        </div>
+        <h3>Best verkochte tv</h3>
+        </div>
+        </>
   )
 }
 
